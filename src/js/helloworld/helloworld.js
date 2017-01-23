0afd4d26f67ad1ff72529ccd389ea4f98fd20135
Elger van Boxtel <elgervb@users.noreply.github.com>
2017-01-23 20:41:18 +0100

    remove hello world

--- src/js/helloworld/helloworld.js
+++
@@ -1,5 +0,0 @@
-import angular from 'angular';
-import HelloworldComponent from './helloworldcomponent';
-
-export default angular.module('helloworld', [])
-    .component('helloworld', new HelloworldComponent());
